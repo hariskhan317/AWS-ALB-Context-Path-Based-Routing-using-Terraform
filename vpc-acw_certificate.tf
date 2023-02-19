cce7b1b8d2a37bc8e8fe7430d12735bb95f2de1b
Haris <harisgujjar317@gmail.com>
2023-02-19 15:57:20 -0500

    acw certificate

---
+++ vpc-acw_certificate.tf
@@ -0,0 +1,21 @@
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> 4.0"
+
+  domain_name  = trimsuffix(data.aws_route53_zone.mydomain.name,".")
+  zone_id      = data.aws_route53_zone.mydomain.zone_id
+
+  subject_alternative_names = [
+    "*.my-domain.com"
+  ]
+
+  tags = {
+    Name = "my-domain.com"
+  }
+}
+
+// output
+output "acm_certificate_arn" {
+  description = "The ARN of the certificate"
+  value       = module.acm.acm_certificate_arn
+}
